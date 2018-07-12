42b603ca70a3ef8483422f7a99e94d0cf33df72f
Sam Goldberg <sgoldber61@gmail.com>
2018-07-12 16:05:39 -0700

    docform route redirect bug fix

--- client/src/components/documents/Settings.js
+++ client/src/components/documents/Settings.js
@@ -3,6 +3,8 @@ import DocForm from './DocForm.js';
 
 const Settings = (props) => {
   let docId = props.location.pathname.slice(10);
-  return <DocForm docId={docId} history={props.history} />
+  return <DocForm docId={docId} mode={'settings'} history={props.history} />
 }
+
 export default Settings;
+
